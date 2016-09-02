ec25022ee93640b7e495522032ae74eec74d4513
stonedwirr <alcohost@gmail.com>
2016-09-02 18:57:47 +0300

    Revert "111"
    This reverts commit ae989f912a926b91c3780d6986f9fd9e9f98e918.

--- Home_Work_4-1.py
+++
@@ -1,7 +0,0 @@
-animal = 'fruitbat'
-def print_global():
-    #print('inside print_global:', animal)
-    animal = 'wombat'
-    print('inside print_global:', animal)
-print('at the top level:', animal)
-print_global()
